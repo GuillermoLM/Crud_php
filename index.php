16ff4d25064eb8b032532bf05e03a1bde6e041e1
Guillermo <you@example.com>
2018-10-09 13:33:25 +0200

    Delete row

--- index.php
+++ index.php
@@ -13,33 +13,40 @@
 
 <body>
     <?php require_once 'process.php'; ?>
-
-    <?php
+    <div class="container">
+        <?php
         $mysqli = new mysqli('localhost','root', '','crud') or die(mysqli_error($mysqli));
         $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
     ?>
 
-    <div class="row justify-content-center">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Location</th>
-                    <th colspan='2'>Action</th>
-                </tr>
-            </thead>
-    <?php
+        <div class="row justify-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th colspan='2'>Action</th>
+                    </tr>
+                </thead>
+                <?php
         while($row = $result->fetch_assoc()): ?>
-            <tr>
-                <td><?php echo $row['name']; ?></td>
-                <td><?php echo $row['location']; ?></td>
-                <td></td>
-            </tr>
-    <?php endwhile; ?>
-        </table>
-    </div>
+                <tr>
+                    <td>
+                        <?php echo $row['name']; ?>
+                    </td>
+                    <td>
+                        <?php echo $row['location']; ?>
+                    </td>
+                    <td>
+                        <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
+                        <a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </table>
+        </div>
 
-    <?php
+        <?php
         function pre_r($array)
         {
             echo '<pre>';
@@ -48,23 +55,24 @@
         }
     ?>
 
-    <div class="row justify-content-center">
-        <div class="col-4"></div>
-        <div class="col-4">
-            <form action="process.php" method="POST">
-                <div class="form-group">
-                    <label>Name</label>
-                    <input type="text" name="name" class="form-control" value="Enter your name">
-                </div>
-                <div class="form-group">
-                    <label>Location</label>
-                    <input type="text" name="location" class="form-control" value="Enter your location">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary" name="save">Save</button>
-                </div>
-            </form>
+        <div class="row justify-content-center">
+            <div class="col-4"></div>
+            <div class="col-4">
+                <form action="process.php" method="POST">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control" value="Enter your name">
+                    </div>
+                    <div class="form-group">
+                        <label>Location</label>
+                        <input type="text" name="location" class="form-control" value="Enter your location">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary" name="save">Save</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-4"></div>
         </div>
-        <div class="col-4"></div>
-    <div>
+    </div>
 </body>
